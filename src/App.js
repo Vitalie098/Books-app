494aa1e3fb51b061608b6359ee0691407aba01a5
Vitalie098 <donceac01@mail.ru>
2021-05-13 20:53:42 +0300

    Finish project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,109 @@
-import logo from './logo.svg';
+import React, {useState} from "react"
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import Listbooks from "./data/books";
+import { Image, ListGroup, ListGroupItem} from "react-bootstrap";
 
+
+function App() {
+
+    const [books, setBooks] = useState(Listbooks)
+
+
+    const onMoveUp = idx => {
+        const booksCopy = [...books]
+        const bookUp = booksCopy[idx]
+
+        for (let i = idx; i < booksCopy.length - 1; i++) {
+            booksCopy[i] = booksCopy[i + 1]
+        }
+
+        booksCopy[books.length - 1] = bookUp
+        setBooks(booksCopy)
+
+    }
+
+    const onMoveDown = idx => {
+        const booksCopy = [...books]
+        const bookDown = booksCopy[idx]
+
+        for (let i = idx; i > 0; i--) {
+            booksCopy[i] = booksCopy[i - 1]
+        }
+        booksCopy[0] = bookDown
+        setBooks(booksCopy)
+    }
+
+    const deleteItem = (e, id) => {
+        e.preventDefault()
+        let booksCopy = [...books]
+        booksCopy = booksCopy.filter(i => i.id !== id)
+
+        setBooks(booksCopy)
+    }
+
+    const changeTitleHandler = (e, id) => {
+        if (e.keyCode === 13) {
+            e.preventDefault();
+            openInput(id)
+            return
+        }
+
+        let booksCopy = [...books]
+        const idx = booksCopy.findIndex(b => b.id === id)
+        booksCopy[idx].title = e.target.value
+
+        setBooks(booksCopy)
+
+    }
+
+    const openInput = id => {
+        let booksCopy = [...books]
+        const idx = booksCopy.findIndex(b => b.id === id)
+        booksCopy[idx].edit = !booksCopy[idx].edit
+
+        setBooks(booksCopy)
+    }
+
+    return (
+        <div className="leaderboard">
+            <header>
+                <h1 className="leaderboard__title"><span className="leaderboard__title--top">Top Books</span>
+                    <span className="leaderboard__title--bottom">The best in the 2021</span></h1>
+            </header>
+            {books.length === 0
+                ? <h2 className="text-center p-3">List is empty</h2>
+                : (
+                    <ListGroup className="leaderboard__books">
+                        {books.map((book, idx) => (
+                                <ListGroupItem
+                                    key={book.id}
+                                    className="leaderboard__book"
+                                    style={{display: "grid"}}
+                                    onDoubleClick={() => onMoveDown(idx)}
+                                    onContextMenu={e => deleteItem(e, book.id)}
+                                >
+                                    <Image
+                                        alt={book.title}
+                                        src={book.src}
+                                        className="leaderboard__picture"
+                                    />
+                                    <span className="leaderboard__name">{book.title}</span>
+                                    <span className="leaderboard__value">
+                                        <i className="fa fa-arrow-down" onClick={() => onMoveUp(idx)} /> {"  "}
+                                        <i className="fas fa-edit" onClick={() => openInput(book.id) }/> </span>
+                                    {book.edit && (<input
+                                        className="editTitle"
+                                        placeholder={book.title}
+                                        onChange={e => changeTitleHandler(e, book.id)}
+                                        onKeyUp = {e => changeTitleHandler(e, book.id)}
+                                    />)}
+                                </ListGroupItem>
+                            )
+                        )}
+                    </ListGroup>
+                )}
+        </div>
+    );
+}
 export default App;
---
+++ src/data/books.js
@@ -0,0 +1,64 @@
+const books = [
+    {
+        title: "Klara and the Sun",
+        src: "/images/1.jpg",
+        edit: false,
+        id: 1
+    },
+    {
+        title: "The Push: A Novel",
+        src: "/images/2.jpg",
+        edit: false,
+        id: 2
+    },
+    {
+        title: "A Crooked Tree: A Novel",
+        src: "/images/3.jpg",
+        edit: false,
+        id: 3
+    },
+    {
+        title: "Let Me Tell You",
+        src: "/images/4.jpg",
+        edit: false,
+        id: 4
+    },
+    {
+        title: "The Wife Upstairs",
+        src: "/images/5.jpg",
+        edit: false,
+        id: 5
+    },
+    {
+        title: "Summerwater: A Novel",
+        src: "/images/6.jpg",
+        edit: false,
+        id: 6
+    },
+    {
+        title: "The Removed: A Novel",
+        src: "/images/7.jpg",
+        edit: false,
+        id: 7
+    },
+    {
+        title: "Girl A: A Novel",
+        src: "/images/8.jpg",
+        edit: false,
+        id: 8
+    },
+    {
+        title: "Caul Baby: A Novel",
+        src: "/images/9.jpg",
+        edit: false,
+        id: 9
+    },
+    {
+        title: "Peaces: A Novel",
+        src: "/images/10.jpg",
+        edit: false,
+        id: 10
+    },
+]
+
+export default books
